805543400f7d2c6beb9dd54586b82f393658c4f8
Thevindu <kulasinghet@gmail.com>
2023-03-04 09:27:55 +0530

    flutter init

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MyApp(),
+      '/dashboard': (context) => Text('Second Route'),
+    },
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Demo'),
+          ),
+          body: child,
+        );
+      },
+      home: Text('First Route'),
+    );
+  }
+}
